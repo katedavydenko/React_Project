b1275dd9ac1ef85592217d98ee88750d37b8f422
katedavydenko <katedavydenko245@gmail.com>
2026-02-18 16:06:42 +0200

    update

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,6 +6,11 @@ function App() {
   const totalAverage = students.reduce((acc, student) => {
     return acc + getAverage(student.grades);
   }, 0) / students.length;
+  const sortedStudents = [...students].sort((a, b) => {
+  const avgA = a.grades.reduce((sum, g) => sum + g, 0) / a.grades.length;
+  const avgB = b.grades.reduce((sum, g) => sum + g, 0) / b.grades.length;
+  return avgB - avgA; 
+});
   return (
     <div className={styles.appContainer}>
       
@@ -18,20 +23,34 @@ function App() {
         ))}
         
       </div>
-      <ul>
+      <div>
           {students
-            // 1. Спочатку фільтруємо масив
             .filter((student) => getAverage(student.grades) > 60)
-            // 2. Потім перетворюємо відфільтровані дані на JSX
             .map((student) => (
               <li key={student.id} style={{ color: 'green' }}>
                 <strong>{student.name}</strong> — Бал: {getAverage(student.grades).toFixed(1)}
               </li>
             ))}
-      </ul>
-        <p>
-        <strong>Середній бал усіх студентів:</strong> {totalAverage.toFixed(2)}
-        </p>
+      </div>
+      <p >Середній бал усіх студентів: {totalAverage.toFixed(2)} </p>
+        <div>
+        {sortedStudents.map((student) => (
+          <li key={student.id} style={{ marginBottom: '8px' }}>
+            {/* Використовуємо тернарний оператор для стилів */}
+            <span style={{
+              color: student.scholarship ? 'black' : 'gray',
+              textDecoration: student.scholarship ? 'none' : 'line-through',
+              fontWeight: student.scholarship ? 'bold' : 'normal'
+            }}>
+              {student.name}
+            </span> 
+            {' — '} 
+            <span>
+              Середній бал: {(student.grades.reduce((a, b) => a + b, 0) / student.grades.length).toFixed(1)}
+            </span>
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
